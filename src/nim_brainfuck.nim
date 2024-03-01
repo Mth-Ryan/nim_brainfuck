3c5520ec5e6ecf70bd217b41bc82699bcdf19bf6
Mateus Ryan <mthryan@protonmail.com>
2024-03-01 19:22:58 -0300

    Add complete interpreter with examples

--- src/nim_brainfuck.nim
+++ src/nim_brainfuck.nim
@@ -1,5 +1,45 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+import os 
+
+{.push overflowchecks: off.}
+proc xinc(c: var char) = inc c
+proc xdec(c: var char) = dec c
+{.pop.}
+
+
+proc load_code(): string =
+  return if paramCount() > 0: readFile paramStr(1)
+         else: readAll stdin
+
+let code = load_code()
+
+var
+  tape: seq[char] = newSeq[char]()
+  codePos: int = 0
+  tapePos: int = 0
+
+proc run(skip = false): bool =
+  while tapePos >= 0 and codePos < code.len:
+    if tapePos >= tape.len:
+      tape.add '\0'
+
+    if code[codePos] == '[':
+      inc codePos
+      let oldPos = codePos
+      while run(tape[tapePos] == '\0'):
+        codePos = oldPos
+    elif code[codePos] == ']':
+      return tape[tapePos] != '\0'
+    elif not skip:
+      case code[codePos]
+      of '+': xinc tape[tapePos]
+      of '-': xdec tape[tapePos]
+      of '>': inc tapePos
+      of '<': dec tapePos
+      of '.': stdout.write tape[tapePos]
+      of ',': tape[tapePos] = stdin.readChar()
+      else: discard
+    
+    inc codePos
 
 when isMainModule:
-  echo("Hello, World!")
+  discard run()
